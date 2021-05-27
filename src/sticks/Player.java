869279c028b3c980298fec620336c5f2cd66c963
poobanaaselvam <poobanaaselvam@gmail.com>
2021-05-27 09:10:07 -0500

    Hand/ Player
    5/ 26/ 2021

--- src/sticks/Player.java
+++ src/sticks/Player.java
@@ -2,4 +2,23 @@ package sticks;
 
 public class Player {
 
+    private Hand RH = new Hand();
+    private Hand LH = new Hand();
+    private String name;
+    
+    public Player(String playerName) {
+        name = playerName;
+    }
+    
+    public void split() {
+        
+    }
+    
+    public boolean dead() {
+        if(RH.dead() && LH.dead())
+            return false;
+        else
+            return true;
+    }
+    
 }
