16d49585d014b02006728e10221f911e7b47d6b8
Anthony Brogni <anthonybrog@gmail.com>
2021-06-03 09:38:16 -0500

    Update Player.java

--- src/sticks/Player.java
+++ src/sticks/Player.java
@@ -27,11 +27,11 @@ public class Player {
     public boolean split() {
         if ((RH.dead() || LH.dead()) && (RH.getFingers() > 1 || LH.getFingers() > 1)) {
             if (RH.dead()) {
-                RH.add(LH.getFingers() / 2);
-                LH.add(LH.getFingers() / 2 * -1);
+                RH.addSplit(LH.getFingers() / 2);
+                LH.addSplit(LH.getFingers() / 2 * -1);
             } else if (LH.dead()) {
-                LH.add(RH.getFingers() / 2);
-                RH.add(RH.getFingers() / 2 * -1);
+                LH.addSplit(RH.getFingers() / 2);
+                RH.addSplit(RH.getFingers() / 2 * -1);
             }
             return true;
         } else {
